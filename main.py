e9df979f583e4d0aef75ce3c4717e2bacc69b0dd
ijinima <ijinima@gmail.com>
2019-06-01 13:38:02 +0430

    run 2

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
+import ds
 
-#get file
+head = ds.node('head',0)
+tree = ds.tree(head,'min_heap')
+
+print(tree)
--- tree.py
+++ tree.py
@@ -5,5 +5,7 @@ class tree:
 
     def __init__(self, head, name='tree'):
         self.name = name
-        self.nodes = []
         self.head = head
+
+    # def printTree(self,head):
+    #     return [head.value] + [self.printTree(head.childs)]
