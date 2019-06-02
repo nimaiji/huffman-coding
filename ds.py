575302c92beafe026feca2680f1867a2cf39d684
ijinima <ijinima@gmail.com>
2019-06-02 16:18:06 +0430

    heapify 1

--- ds.py
+++ ds.py
@@ -1,3 +1,11 @@
+import enum
+
+
+class dir(enum.Enum):
+    LEFT = 0
+    RIGHT = 1
+
+
 class node:
 
     def __init__(self, name="node", value=0):
@@ -5,6 +13,8 @@ class node:
         self.value = value
         self.childs = []
         self.parents = []
+        self.dir = None
+        self.headState = False
 
     def addChild(self, child):
         self.childs.append(child)
@@ -23,7 +33,16 @@ class node:
                 self.parents.remove(parent)
 
     def isHead(self):
-        return self.parents == []
+        return self.headState
+
+    def setHead(self):
+        self.headState = True
+
+    def unsetHead(self):
+        self.headState = False
+
+    def __str__(self):
+        return self.name + " : " + str(self.value)
 
 
 class tree:
@@ -31,8 +50,12 @@ class tree:
     def __init__(self, head, name='tree', isPrefect=False):
         self.name = name
         self.isPrefect = isPrefect
+        self.nodes = []
+        self.size = len(self.nodes)
         if (head.isHead):
             self.head = head
+            head.setHead()
+            self.nodes.append(head)
         else:
             raise Exception("Illegal node")
 
@@ -42,5 +65,37 @@ class tree:
 
 class minHeap(tree):
 
-    def __init__(self, head, name='min heap', isPrefect=False):
+    def __init__(self, head, name='min heap', isPrefect=True):
         super().__init__(head, name, isPrefect)
+
+    def insertNode(self, newNode):
+        # new node index
+        cuIndex = len(self.nodes)
+        # defining direction
+        if (cuIndex % 2 == 0):
+            newNode.dir = dir.RIGHT
+        else:
+            newNode.dir = dir.LEFT
+
+        self.nodes.append(newNode)
+        self.heapify(cuIndex)
+
+    def heapify(self, index):
+        cuNode = self.nodes[index]
+        if (cuNode.dir == dir.LEFT):
+            paNode = self.nodes[int((index - 1) / 2)]
+        else:
+            paNode = self.nodes[int((index - 2) / 2)]
+
+        if (cuNode.value < paNode.value):
+
+            # Swap Nodes
+            cuNode.value, paNode.value = paNode.value, cuNode.value
+            cuNode.name, paNode.name = paNode.name, cuNode.name
+
+            # If current node is head do not continue heapify
+            if (not paNode.isHead()):
+                if (cuNode.dir == dir.LEFT):
+                    self.heapify(int((index - 1) / 2))
+                else:
+                    self.heapify(int((index - 2) / 2))
--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 import ds
 
-head = ds.node('head',0)
-tree = ds.tree(head,'min_heap')
+#file input
+# quantity = {}
+# file = open('./input.txt','r')
+# for line in file:
+#     for index,char in enumerate(line):
+#         #print(index,char)
+#         if (char in quantity):
+#             quantity.update({char:quantity[char]+1})
+#         else:
+#             quantity[char] = 1
 
-min = ds.minHeap(head)
-print(min.name)
+
+
+head = ds.node('head',20)
+n1 = ds.node('n1',17)
+n2 = ds.node('n2',5)
+n3 = ds.node('n3',10)
+n4 = ds.node('n4',4)
+n5 = ds.node('n3',12)
+
+minHeap = ds.minHeap(head)
+minHeap.insertNode(n1)
+minHeap.insertNode(n2)
+minHeap.insertNode(n3)
+minHeap.insertNode(n4)
+minHeap.insertNode(n5)
+for x in minHeap.nodes:
+    print(x.value)
